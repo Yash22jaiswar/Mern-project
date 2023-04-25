83e8804a965d8adb5020a38d10d30877e139a5a9
Yash Jaiswar <jaiswar.yash7@gmail.com>
2023-04-25 19:11:03 +0530

    conn.js

--- mern/server/db/conn.js
+++ mern/server/db/conn.js
@@ -13,7 +13,7 @@ module.exports = {
       // Verify we got a good "db" object
       if (db)
       {
-        _db = db.db("BACKEND");
+        _db = db.db("MernProject");
         console.log("Successfully connected to MongoDB."); 
       }
       return callback(err);
